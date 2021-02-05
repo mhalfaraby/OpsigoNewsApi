b1a2e702548b09f6566af660d175b9df7b9cd68c
Muhammad Alfaraby <mhalfaraby@gmail.com>
2021-02-05 13:52:30 +0700

    Initial Commit

---
+++ OpsigoNewsApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpsigoNewsApi
+//
+//  Created by MUHAMMAD ALFARABY on 05/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
