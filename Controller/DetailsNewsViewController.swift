14649ab8c22c113319724982a863dee312febc7c
Muhammad Alfaraby <mhalfaraby@gmail.com>
2021-02-07 20:35:24 +0700

    links click

--- Controller/DetailsNewsViewController.swift
+++ Controller/DetailsNewsViewController.swift
@@ -19,17 +19,20 @@ class DetailsNewsViewController: UIViewController {
   @IBOutlet weak var link: UILabel!
   @IBOutlet weak var time: UILabel!
   @IBOutlet weak var authorName: UILabel!
-  @IBOutlet weak var sources: UILabel!
+  @IBOutlet weak var links: UITextView!
   
   //MARK: -  viewDidLoad
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    links.isEditable = false
+    links.dataDetectorTypes = UIDataDetectorTypes.all
+    
     detailTitle.text = detailNews?.title
     detailDescription.text = detailNews?.description
     time.text = detailNews?.publishedAt
     authorName.text = detailNews?.source.name
-    link.text = detailNews?.url
+    links.text = detailNews?.url
     
     if let image = detailNews?.urlToImage {
       let urlToImage = NSURL.init(string: image)
